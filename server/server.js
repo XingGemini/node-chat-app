e72f71baceba42eee6ecd8fed6702fc397071b1f
Xing Xu <xing@dashgenomic.com>
2017-08-01 16:45:00 -0700

    create new user join and welcome event

--- server/server.js
+++ server/server.js
@@ -20,6 +20,18 @@ io.on('connect',  (socket) => {
   //   createdAt: 333
   // });
 
+  socket.emit ('newMessage', {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit ('newMessage', {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
   socket.on ('createMessage', (message) => {
     console.log("create a new message", message);
     io.emit ('newMessage', {
@@ -27,6 +39,12 @@ io.on('connect',  (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit ('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on ('disconnect', () => {
